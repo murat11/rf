38525ad222efdfdbe57b29df171e8b28ea7f819a
Murat Erkenov <murat.erkenov@ipresence.ca>
2019-10-14 17:20:46 +0300

    SalaryCalculationRequest tested

---
+++ tests/Unit/Domain/Salary/SalaryCalculationRequestTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Test\Unit\Domain\Salary;
+
+use App\Domain\Salary\SalaryCalculationRequest;
+use PHPUnit\Framework\TestCase;
+
+class SalaryCalculationRequestTest extends TestCase
+{
+    public function testItIsInitializedProperly()
+    {
+        $request = new SalaryCalculationRequest('John Doe', 1000, 30, 5, true);
+        $this->assertEquals('John Doe', $request->getName());
+        $this->assertEquals(1000, $request->getGrossSalary());
+        $this->assertEquals(30, $request->getAge());
+        $this->assertEquals(5, $request->getNumberOfKids());
+        $this->assertTrue($request->isUsesCompanyCar());
+    }
+}
